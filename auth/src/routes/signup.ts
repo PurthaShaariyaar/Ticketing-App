c1c38a0d32b8f0f87bcbde2996ec5724e3b71945
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-02-28 02:13:22 -0500

    Implemented validation via express-validator for auth service. Need to implement an error handling middleware.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,14 +1,41 @@
 // Import required modules
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 // Create an express router
 const router = express.Router();
 
 /**
- * Router.post call
+ * Router.post for user sign up
+ * Use express validator { body } as a middleware for validation
+ * Get the errors via validationRequest
+ * Check if errors !empty -> if so -> send a status of 400 and send errors back as array
+ * Extract the content from the body -> email, password = req.body;
  */
-router.post('/api/users/signup', (req, res) => {
-  res.send('Sign up.');
+router.post('/api/users/signup',
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).send(errors.array());
+  }
+
+  const { email, password } = req.body;
+
+  console.log('Creating a user...');
+
+  res.send({});
+
 });
 
 // Export the router
