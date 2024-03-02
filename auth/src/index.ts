82471479b21a97e8f060b772294b32d80bd8d7ba
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-03-02 17:35:16 -0500

    Finished functionality to connecto to MongoDB. Had to fix the dependency packages to downgrade mongoose versions.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,15 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
 
 // Create an express application
 const app = express();
@@ -23,13 +26,34 @@ app.use(signoutRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 
+// Throw a new error if route not found -> it will send to error handling middleware
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
+// Format the error received in a specific format -> errorHandler
 app.use(errorHandler);
 
-// Start the server and listen on port 3000
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
-});
+/**
+ * Connect to mongoose
+ * Mongoose will connect to a MongoDB url
+ * The url should be the k8s mongo service name with the port number with the actual database name
+ * If you create your own database name -> '/auth' -> mongoose will automatically create it
+ * Use try catch block -> try to connect to db -> catch error if connection fails
+ * Only start the server and listen on port 3000 after connection is successful
+ */
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
+// Call start() when the app initially runs
+start();
